8c3ab5ccb0ddc818280339dc8a9d1604d0edb7c7
Michael Watts <wattsjmichael@gmail.com>
2020-09-11 11:23:17 -0700

    equations are now showing value for AED

--- src/main.js
+++ src/main.js
@@ -4,26 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $("#currencyConverter").click(function(){
-    const usdValue = $("#usdConverter").val();
-    $("#usdConverter").val("");
+  $("#usdSubmit").click(function(){
+    const usdValue = $("#usdValue").val();
+    $("#usdValue").val("");
     console.log(usdValue);
 
+
     let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
     request.onreadystatechange = function() {
       if(this.readyState === 4 && this.status === 200) {
         const response = JSON.parse (this.responseText);
         getElements(response);
       }
-    }
+    };
+
     request.open("GET", url, true);
     request.send();
 
+
     function getElements(response) {
-      $(".showUSD").html(`USD Dollars: ${response.conversion_rates.USD}` * (usdValue));
-      $(".showAED").html(`USD Dollars: ${response.conversion_rates.AED}` * (usdValue));
+      $(".showUSD").html((`USD Dollars: ${(response.conversion_rates.USD)*(usdValue)}`));
+      $(".showAED").text((`AED Value: ${(response.conversion_rates.AED)*(usdValue)}`));
     }
-  })
+
+
+  });
 });
