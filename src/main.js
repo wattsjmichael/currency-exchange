c8ffc80c55e2683e58704831d2b92f3aaf1a84e5
Michael Watts <wattsjmichael@gmail.com>
2020-09-11 14:07:34 -0700

    adds functionality for selecting USD or AED Value

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
   $("#usdSubmit").click(function(){
     let usdValue = $("#usdValue").val();
     $("#usdValue").val("");
-    $()
 
 
 
@@ -30,10 +29,34 @@ $(document).ready(function() {
 
 
     function getElements(response) {
+      let country = $("#country").val();
+      console.log(country);
+      if (country === "USD"){
+        $(".showAED").text((`USD Value: ${(response.conversion_rates.USD)*(usdValue)}`)).show();
+      } else if (country === "AED"){
+        $(".showAED").text((`AED Value: ${(response.conversion_rates.AED)*(usdValue)}`)).show();
+      } else {
+        alert ("its so broken!")
+      }     
       
-      $(".showUSD").html((`USD Dollars: ${(response.conversion_rates.USD)*(usdValue)}`));
-      $(".showAED").text((`AED Value: ${(response.conversion_rates.AED)*(usdValue)}`));
+      
+      
+      
+      // $("#country > option").foreach(function(){
+      //   console.log(this.value);
+      //   if (this.value === "AED") {
+      //     $(".showAED").text((`AED Value: ${(response.conversion_rates.AED)*(usdValue)}`)).show();
+      //   } else if (this.value === "USD") {
+      //     $(".showUSD").text((`USD Value: ${(response.conversion_rates.USD)*(usdValue)}`)).show();
+      //   }
+        
+
+      // });
     }
+
+    //   $(".showUSD").html((`USD Dollars: ${(response.conversion_rates.USD)*(usdValue)}`));
+    //   $(".showAED").text((`AED Value: ${(response.conversion_rates.AED)*(usdValue)}`));
+    // }
 
 
   });
