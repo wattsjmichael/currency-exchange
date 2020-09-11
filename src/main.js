1907a5d5bb026df3feefc101de7fccef4e76ed70
Michael Watts <wattsjmichael@gmail.com>
2020-09-11 16:44:11 -0700

    conversion-service.js broke the website

--- src/main.js
+++ src/main.js
@@ -2,34 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ConversionService from './conversion-service.js';
 
 $(document).ready(function() {
   $("#usdSubmit").click(function(){
     let usdValue = $("#usdValue").val();
+    let country = $("#country").val();
     $("#usdValue").val("");
   $("#reload").click(function(){
     location.reload();
   });
 
-
-let promise = new Promise(function(resolve, reject){
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-  request.onload = function() {
-    if (this.status === 200){
-    resolve(request.response);
-  } else {
-    reject(request.response);
-  }
-}
-request.open("Get", url, true);
-request.send();
-});
-
+  let promise = ConversionService.getConversion(usdValue);
     promise.then(function(response) {
-      let country = $("#country").val();
       const body = JSON.parse(response);
-      console.log(`${error}`)
         if (country === "USD"){
           $("#showUSD").text((`USD Value: ${(body.conversion_rates.USD)*(usdValue)}`)).show();
           console.log(`${body.conversion_rates.USD}`)
@@ -44,10 +30,10 @@ request.send();
         } else if (country === "BRL"){
           $("#showBRL").text((`BRL Value: ${(body.conversion_rates.BRL)*(usdValue)}`)).show();
         } else {
-          $("#showError").text("Pick a different country!");
+          $("#showError").text("Pick a different country!").show;
         }
       }, function(error) {
-          $("#showError").text(`There was an error processing your request; ${error}`);
+          $("#showError").text(`There was an error processing your request; ${error}`).show();
 
         });
       });
