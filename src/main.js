65eab6cee112b5239e5277f4f48c487f35984bec
Michael Watts <wattsjmichael@gmail.com>
2020-09-11 09:15:51 -0700

    API request is blocked by CORS header, more research is needed

--- src/main.js
+++ src/main.js
@@ -4,4 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
+  $("#usdConverter").click(function(){
+    const country = $("#currencyConverter").val();
+    $("#currencyConverter").val("");
+
+    let request = new XMLHttpRequest();
+    const url = ` https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/${country}`;
+
+    request.onreadystatechange = function() {
+      if(this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse (this.responseText);
+        getElements(response);
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $("#showUSD").html(`USD Dollars: ${response.conversion_rates.usd}`);
+      console.log(response.conversion_rates.usd);
+    }
+  })
 });
