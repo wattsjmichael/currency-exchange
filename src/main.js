26825968739d25da8e62019cc3676c4833e8a4de
Michael Watts <wattsjmichael@gmail.com>
2020-09-12 15:35:05 -0700

    website works, cannot get ASYNC to work properly

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
   let promise = new Promise(function(resolve, reject){
     let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-ap.com/v6/${process.env.API_KEY}/latest/${place}`;
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${place}`;
 
       request.onload = function(){
       if (this.status === 200){
