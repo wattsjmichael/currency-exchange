535d0d1fe28b71584f4f033ce43e16451e2be08b
Michael Watts <wattsjmichael@gmail.com>
2020-09-12 14:19:16 -0700

    adds Promise Functionality

--- src/main.js
+++ src/main.js
@@ -12,37 +12,44 @@ $(document).ready(function(){
     $("#country").val("");
   
 
+
+
+  let promise = new Promise(function(resolve, reject){
     let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${place}`;
+    const url = `https://v6.exchangerate-ap.com/v6/${process.env.API_KEY}/latest/${place}`;
 
-    request.onreadystatechange = function(){
-      if(this.readyState === 4 && this.status === 200){
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+      request.onload = function(){
+      if (this.status === 200){
+        resolve(request.response);
+      } else {
+        reject(request.response);
       }
-    };
+      }
     
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response){
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(response){
+      const body = JSON.parse(response);
       if (place === "USD"){
-        $("#showUSD").text(`${(response.conversion_rates.USD * usdValue)} Dollars`).show();
+        $("#showUSD").text(`${(body.conversion_rates.USD * usdValue)} Dollars`).show();
       } else if (place === "AED") {
-        $("#showAED").text(`${(response.conversion_rates.USD * usdValue)} Dirham`).show();
+        $("#showAED").text(`${(body.conversion_rates.USD * usdValue)} Dirham`).show();
       } else if (place === "ARS") {
-        $("#showARS").text(`${(response.conversion_rates.USD * usdValue)} Argentine Pesos`).show();
+        $("#showARS").text(`${(body.conversion_rates.USD * usdValue)} Argentine Pesos`).show();
       } else if (place === "AUD") {
-        $("#showAUD").text(`${(response.conversion_rates.USD * usdValue)} Australian Dollar`).show();
+        $("#showAUD").text(`${(body.conversion_rates.USD * usdValue)} Australian Dollar`).show();
       } else if (place === "BGN") {
-        $("#showBGN").text(`${(response.conversion_rates.USD * usdValue)} Bulgarian Lev`).show();
+        $("#showBGN").text(`${(body.conversion_rates.USD * usdValue)} Bulgarian Lev`).show();
       } else if (place === "BRL") {
-        $("#showBRL").text(`${(response.conversion_rates.USD * usdValue)} Brazillian Real`).show();
+        $("#showBRL").text(`${(body.conversion_rates.USD * usdValue)} Brazillian Real`).show();
       } else if (place === null) {
         $("#showError").text("Please Select a Country").show();
       } else {
-        alert(`If you made it here, call tech support error ${response.status}`)
+        alert(`If you made it here, call tech support error ${body.status}`)
       }
-    }
+    }, function(status) {
+      $("#showError").text(`There was a error ${status}`)
+    });
   });
 });
