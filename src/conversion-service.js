cbe9465e0383bb8436855a7ae6095d5fbe2acd48
Michael Watts <wattsjmichael@gmail.com>
2020-09-11 17:52:31 -0700

    Fetch functionality broke the website. Trying to troubleshoot it

--- src/conversion-service.js
+++ src/conversion-service.js
@@ -1,18 +1,15 @@
-export default class ConversionService{
-  static getConversion(usdValue) {
-    return new promise = new Promise(function(resolve, reject){
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function() {
-        if (this.status === 200){
-        resolve(request.response);
-      } else {
-        reject(request.response);
+export default class ConversionService {
+  static getConversion() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    .then(function(response) {
+      if(!response.ok) {
+        throw Error(response.statusText);
       }
-    }
-    request.open("Get", url, true);
-    request.send();
-    });
-    
+      return response.json();
+    })
+    .catch(function(error){
+      return error
+    })
   }
 }
+
--- src/main.js
+++ src/main.js
@@ -4,37 +4,78 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ConversionService from './conversion-service.js';
 
-$(document).ready(function() {
-  $("#usdSubmit").click(function(){
-    let usdValue = $("#usdValue").val();
-    let country = $("#country").val();
-    $("#usdValue").val("");
-  $("#reload").click(function(){
-    location.reload();
+
+let usdValue = $("#usdValue").val();
+let country = $("#country").val();
+function getElements(response) {
+  if (response) {
+    if (country === "USD") {
+    $("#showUSD").text((`USD Value: ${(response.conversion_rates.USD)*(usdValue)}`)).show();
+     
+      console.log(usdValue);
+  } else {
+    $("#showError").text(`There was an error ${response.status}`);
+  }
+}
+}
+$(document).ready(function(){
+  ("#usdSubmit").click(function(){
+    ConversionService.getConversion()
+    .then(function(response) {
+      getElements(response);
+    });
+    });
   });
 
-  let promise = ConversionService.getConversion(usdValue);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-        if (country === "USD"){
-          $("#showUSD").text((`USD Value: ${(body.conversion_rates.USD)*(usdValue)}`)).show();
-          console.log(`${body.conversion_rates.USD}`)
-        } else if (country === "AED"){
-          $("#showAED").text((`AED Value: ${(body.conversion_rates.AED)*(usdValue)}`)).show();
-        } else if (country === "ARS"){
-          $("#showARS").text((`ARS Value: ${(body.conversion_rates.ARS)*(usdValue)}`)).show();
-        } else if (country === "AUD"){
-          $("#showAUD").text((`AUD Value: ${(body.conversion_rates.AUD)*(usdValue)}`)).show();
-        } else if (country === "BGN"){
-          $("#showBGN").text((`BGN Value: ${(body.conversion_rates.BGN)*(usdValue)}`)).show();
-        } else if (country === "BRL"){
-          $("#showBRL").text((`BRL Value: ${(body.conversion_rates.BRL)*(usdValue)}`)).show();
-        } else {
-          $("#showError").text("Pick a different country!").show;
-        }
-      }, function(error) {
-          $("#showError").text(`There was an error processing your request; ${error}`).show();
 
-        });
-      });
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("#usdSubmit").click(function(){
+//     let usdValue = $("#usdValue").val();
+//     let country = $("#country").val();
+//     $("#usdValue").val("");
+//   $("#reload").click(function(){
+//     location.reload();
+//     ConversionService.getConversion()
+//     .then(function(response){
+//       getElements(response);
+//   });
+
+
+// function getConversion(response){
+//   if (response) {
+//         if (country === "USD"){
+//           $("#showUSD").text((`USD Value: ${(response.main.conversion_rates.USD)*(usdValue)}`)).show();
+//           console.log(`${response.main.conversion_rates.USD}`)
+//         } else if (country === "AED"){
+//           $("#showAED").text((`AED Value: ${(response.main.conversion_rates.AED)*(usdValue)}`)).show();
+//         } else if (country === "ARS"){
+//           $("#showARS").text((`ARS Value: ${(response.main.conversion_rates.ARS)*(usdValue)}`)).show();
+//         } else if (country === "AUD"){
+//           $("#showAUD").text((`AUD Value: ${(response.main.conversion_rates.AUD)*(usdValue)}`)).show();
+//         } else if (country === "BGN"){
+//           $("#showBGN").text((`BGN Value: ${(response.main.conversion_rates.BGN)*(usdValue)}`)).show();
+//         } else if (country === "BRL"){
+//           $("#showBRL").text((`BRL Value: ${(response.main.conversion_rates.BRL)*(usdValue)}`)).show();
+//         } else {
+//           $("#showError").text("Pick a different country!").show;
+//         }
+//       } else {
+//           $("#showError").text(`There was an error processing your request; ${error}`).show()
